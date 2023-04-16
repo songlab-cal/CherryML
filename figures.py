fee11ec58f8a4eed8a87d97ce468cbfc70dd3b9e
Sebastian Prillo <10426884+sprillo@users.noreply.github.com>
2023-04-16 12:25:37 -0700

    Need to import get_msa_num_sites

--- figures.py
+++ figures.py
@@ -60,6 +60,7 @@ from cherryml.evaluation import (
 )
 from cherryml.global_vars import TITLES
 from cherryml.io import (
+    get_msa_num_sites,
     read_contact_map,
     read_log_likelihood,
     read_mask_matrix,
