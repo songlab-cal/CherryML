0314d4f8b07294ca16264b4f8d08012f8f55705e
Sebastian Prillo <10426884+sprillo@users.noreply.github.com>
2023-09-13 11:15:06 -0700

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
 
-# Import version
-__builtins__.__CHERRYML_SETUP__ = True
-from cherryml import __version__ as version
+def get_version_from_init(init_path='cherryml/__init__.py'):
+    with open(init_path, 'r') as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return eval(line.split('=')[-1])
 
+
+version = get_version_from_init()
 
 setup(
     name='cherryml',
