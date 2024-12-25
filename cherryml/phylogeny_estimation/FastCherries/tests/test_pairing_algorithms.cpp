42506a745513cc1564f191b1e70b5c789b2376ed
uWnosliW <wilson.wu.002@gmail.com>
2024-12-24 19:55:28 -0800

    fixed bug in old test, added more sanity tests comparing copied code to original codebase

--- cherryml/phylogeny_estimation/FastCherries/tests/test_pairing_algorithms.cpp
+++ cherryml/phylogeny_estimation/FastCherries/tests/test_pairing_algorithms.cpp
@@ -18,6 +18,23 @@ std::vector<int> string_to_ints(const std::string& x) {
     return res;
 }
 
+void test_dc_pairer(
+    const std::vector<std::string> &msa_list,
+    const std::unordered_map<std::string, std::vector<int> > &msa_map,
+    const std::vector<std::pair<std::string, std::string> > &expected,
+    const std::string success_message
+) {
+    std::vector<std::pair<std::string, std::string> > res = divide_and_pair(msa_list, msa_map);
+
+    assert(res.size() == expected.size());
+    for(int i = 0; i < res.size(); i++) {
+        
+        assert(expected[i] == res[i]);
+    }
+
+    std::cout << success_message << std::endl;
+}
+
 void test_dc_pair_with_hamming_distance() {
     srand(1);
     std::vector<std::string> msa_list = {"a", "b", "c", "d", "e", "f", "a1", "b1", "c1", "d1", "e1", "f1"};
@@ -35,23 +52,58 @@ void test_dc_pair_with_hamming_distance() {
     msa_map["e1"] = string_to_ints("CDCDCDCDCD");
     msa_map["f1"] = string_to_ints("DCDCDCDCDC");
 
-    std::vector<std::pair<std::string, std::string> > expected = {{"a", "b"},{"c", "d"}, {"e", "f"},{"a1", "b1"},{"c1", "d1"}, {"e1", "f1"}};
+    std::vector<std::pair<std::string, std::string> > expected = {{"e", "e1"}, {"f", "f1"}, {"d", "d1"}, {"c", "c1"}, {"b", "b1"}, {"a", "a1"} };
 
-    std::vector<std::pair<std::string, std::string> > res = divide_and_pair(msa_list, msa_map);
-    
-    srand(1); 
-    std::vector<std::pair<std::string, std::string> > res1 = divide_and_pair(msa_list, msa_map);
-    
-    for(int i = 0; i < res.size(); i++) {
-        bool match = (res[0] == res1[0] && res[1] == res1[1]) ||  (res[0] == res1[1] && res[1] == res1[0]);
-        assert(match);
-    }
-    assert(res.size() == expected.size());
+    test_dc_pairer(msa_list, msa_map, expected, "sanity 1 passed");
+}
 
-    std::cout << "dc with hamming distance sanity passed" << std::endl;
+void test_dc_pair_with_hamming_distance2() {
+    srand(1);
+    std::vector<std::string> msa_list = {"a", "b", "c", "d", "e", "f", "a1", "b1", "c1", "d1", "e1", "f1"};
+    std::unordered_map<std::string, std::vector<int> > msa_map;  
+    msa_map["a"] = string_to_ints("QQQEEECQ");
+    msa_map["b"] = string_to_ints("EEEQQQCC");
+    msa_map["c"] = string_to_ints("EEQQCCQC");
+    msa_map["d"] = string_to_ints("CEEQECCE");
+    msa_map["e"] = string_to_ints("EEQQEEQE");
+    msa_map["f"] = string_to_ints("EEQQCCCE");
+    msa_map["a1"] = string_to_ints("EEQQEECE");
+    msa_map["b1"] = string_to_ints("EEEQQQCE");
+    msa_map["c1"] = string_to_ints("CCEEQQQQ");
+    msa_map["d1"] = string_to_ints("CEEQECQE");
+    msa_map["e1"] = string_to_ints("QQQEEEEC");
+    msa_map["f1"] = string_to_ints("EEQQCCQC");
+
+    std::vector<std::pair<std::string, std::string> > expected = {{"a", "e1"}, {"b", "b1"}, {"c", "f1"}, {"d1", "f"}, {"e", "a1"}, {"c1", "d"}};
+
+    test_dc_pairer(msa_list, msa_map, expected, "sanity 2 passed");
+}
+
+void test_dc_pair_with_hamming_distance3() {
+    srand(1);
+    std::vector<std::string> msa_list = {"a", "b", "c", "d", "e", "f", "a1", "b1", "c1", "d1", "e1", "f1"};
+    std::unordered_map<std::string, std::vector<int> > msa_map;  
+    msa_map["a"] = string_to_ints("TWYWYTWYWYWTYWYW");
+    msa_map["b"] = string_to_ints("TYWYWTYTWWYTWYWW");
+    msa_map["c"] = string_to_ints("YWYWTYWYTWYTWYWY");
+    msa_map["d"] = string_to_ints("WYTWYWYTTWYWTYTT");
+    msa_map["e"] = string_to_ints("YTWYTWTWTWTTTWWY");
+    msa_map["f"] = string_to_ints("YYYYTWTWTTWTWWYW");
+    msa_map["a1"] = string_to_ints("WYTYWTWTTWYYWYWY");
+    msa_map["b1"] = string_to_ints("WTYYYWYWWWYTWTTT");
+    msa_map["c1"] = string_to_ints("YTYYTYWWYTWTWWWW");
+    msa_map["d1"] = string_to_ints("WYYTWYWYTWTWWTTT");
+    msa_map["e1"] = string_to_ints("YTWYTYWWTYTWYTWY");
+    msa_map["f1"] = string_to_ints("TYTYTYYTWTWWWTWW");
+
+    std::vector<std::pair<std::string, std::string> > expected = {{"a", "f"}, {"c", "c1"}, {"b", "e"}, {"d", "a1"}, {"d1", "b1"}, {"e1", "f1"}};
+
+    test_dc_pairer(msa_list, msa_map, expected, "sanity 3 passed");
 }
 int main() {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     test_dc_pair_with_hamming_distance();
+    test_dc_pair_with_hamming_distance2();
+    test_dc_pair_with_hamming_distance3();
     return 0;
 }
  
