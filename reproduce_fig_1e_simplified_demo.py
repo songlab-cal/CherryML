cbe69c4b09ad0f7b4423804a85900ba35a345bf9
uWnosliW <wilson.wu.002@gmail.com>
2025-01-12 00:21:16 -0800

    fixed reproduce_fig_1e_simplified_demo.py to reproduce the original cherryml figure 1e (with color differences) using the new plotting code

--- reproduce_fig_1e_simplified_demo.py
+++ reproduce_fig_1e_simplified_demo.py
@@ -8,22 +8,18 @@ if __name__ == "__main__":
     figures.fig_lg_paper(
         evaluation_phylogeny_estimator_name="FastTree",
         output_image_dir="fig_1e_simplified/",
-        num_processes=9,
+        num_processes=32,
         rate_estimator_names=[
-            #("reproduced WAG", "WAG\nrate matrix"),
-            #("reproduced LG", "LG\nrate matrix"),
-            ("Cherry++__4__FastCherries_4rc", "LG model\n CherryML\n with \nFastCherries 4rc"),
-            #("Cherry++__4__FastTree_4rc", "LG model\n CherryML\n with \nFastTree 4rc"),
+            ("reproduced WAG", "WAG\nrate matrix"),
+            ("reproduced LG", "LG\nrate matrix"),
+            ("Cherry++__4__FastTree_4rc", "LG model\n CherryML\n with \nFastTree 4rc"),
         ],
         phylogeny_estimator_configs=[
-            #create_config_from_dict({"identifier":"fast_tree", "args":{"num_rate_categories":1}}),
-            #create_config_from_dict({"identifier":"fast_tree", "args":{"num_rate_categories":1}}),
-            create_config_from_dict({"identifier":"fast_cherries", 
-                                    "args":{"num_rate_categories":4, "max_iters":50, "_version":3}}
+            create_config_from_dict({"identifier":"", "args":{}}),
+            create_config_from_dict({"identifier":"", "args":{}}),
+            create_config_from_dict({"identifier":"fast_tree", 
+                                    "args":{"num_rate_categories":4}}
                                     ),
-            #create_config_from_dict({"identifier":"fast_tree", 
-            #                        "args":{"num_rate_categories":4}}
-            #                        ),
         ],
         lg_pfam_training_alignments_dir="data/lg_paper_data/lg_PfamTrainingAlignments",
         lg_pfam_testing_alignments_dir="data/lg_paper_data/lg_PfamTestingAlignments",
